7caac7bfffb3068275f4fb353df75974b2644dc0
Tamim <kaytotamim@gmail.com>
2024-02-11 16:56:30 +0100

    eq

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
 // Import necessary modules
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config(); // Load environment variables
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config(); // Load environment variables
 
 // Import routers
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var participantRouter = require('./routes/participant');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var participantRouter = require("./routes/participant");
 
 // AWS DynamoDB configuration
-const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 
 const dynamoDBClient = new DynamoDB({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 // Initialize Express app
 var app = express();
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/participant', participantRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/participant", participantRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // Export the app
