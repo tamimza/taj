9abadb29c06a5ddd322e67d26433d50d03eab479
Tamim <kaytotamim@gmail.com>
2024-02-11 17:31:33 +0100

    edd

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const dynamoDBClient = new DynamoDB({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
   },
 });
 
